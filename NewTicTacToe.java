eb79c85365e8599b54a29fd3826b86d4d1abb1a2
Riya Kushwaha <riyakushwaha253@gmail.com>
2024-01-13 13:28:01 +0530

    update computer vs player

--- NewTicTacToe.java
+++ NewTicTacToe.java
@@ -23,15 +23,15 @@ public class NewTicTacToe implements ActionListener {
 
     boolean vsComputerMode = false;
     boolean player1_turn;
-    Map<String, Integer> scoreboard = new HashMap<>();
+    private Map<String, Integer> scoreboard = new HashMap<>();
 
     NewTicTacToe() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(490, 685);
-        frame.getContentPane().setBackground(new Color(50, 50, 50));
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
-
+        frame.setTitle("Riya - TIC TAC TOE Game");
+        
         textfield.setBackground(new Color(25, 25, 25));
         textfield.setForeground(new Color(25, 255, 0));
         textfield.setFont(new Font("Ink Free", Font.BOLD, 35));
@@ -40,7 +40,6 @@ public class NewTicTacToe implements ActionListener {
         textfield.setOpaque(true);
         int verticalGap = 20;
         textfield.setBorder(BorderFactory.createEmptyBorder(verticalGap, 0, verticalGap, 0));
-
 
         title_panel.setLayout(new BorderLayout());
         title_panel.setBounds(0, 0, 800, 100);
@@ -126,6 +125,7 @@ public class NewTicTacToe implements ActionListener {
         vsPlayerButton.addActionListener(this);
 
         firstTurn();
+        resetScoreboard();
 
     }
 
@@ -137,6 +137,7 @@ public class NewTicTacToe implements ActionListener {
             System.exit(0);
         } else if (e.getSource() == vsComputerButton) {
             vsComputerMode = true;
+            resetScoreboard();
             resetGame();
             textfield.setText("Player (X) vs Computer (O)");
             if (!player1_turn) {
@@ -144,6 +145,7 @@ public class NewTicTacToe implements ActionListener {
             }
         } else if (e.getSource() == vsPlayerButton) {
             vsComputerMode = false;
+            resetScoreboard();
             resetGame();
             textfield.setText("Player 1 (X) vs Player 2 (O)");
         } else {
@@ -157,14 +159,11 @@ public class NewTicTacToe implements ActionListener {
                 }
             }
         }
-    }
-    
-    
+    }    
     
     private boolean playerMoveMade = false;
     
-
- // Modify handleVsComputerMove() method
+    
     private void handleVsComputerMove(int index) {
         if (!playerMoveMade && buttons[index].getText().equals("")) {
             buttons[index].setForeground(new Color(255, 0, 0));
@@ -178,7 +177,7 @@ public class NewTicTacToe implements ActionListener {
         }
     }
 
-    // Modify handleVsPlayerMove() method
+  
     private void handleVsPlayerMove(int index) {
         if (player1_turn && buttons[index].getText().equals("")) {
             buttons[index].setForeground(new Color(255, 0, 0));
@@ -203,29 +202,30 @@ public class NewTicTacToe implements ActionListener {
     
     private void computerMove() {
         int index = -1;
-
-        // Check if player clicked on button 3
-        if (buttons[2].getText().equals("X")) {
+        
+        // Check if player clicked on button 1
+        if (buttons[0].getText().equals("X")) {
             // If button 5 is not selected, make a move on button 5
             if (!buttons[4].getText().equals("")) {
-                // If button 1 is not selected, make a move on button 1
-                if (!buttons[0].getText().equals("")) {
-                    // If button 3 is not selected, make a move on button 3
-                    if (!buttons[2].getText().equals("")) {
+                // If button 2 is not selected, make a move on button 2
+                if (!buttons[1].getText().equals("")) {
+                    // If button 4 is not selected, make a move on button 4
+                    if (!buttons[3].getText().equals("")) {
                         // If none of the preferred moves are available, make a random move
                         do {
                             index = random.nextInt(9);
                         } while (!buttons[index].getText().equals(""));
                     } else {
-                        index = 2;
+                        index = 3;
                     }
                 } else {
-                    index = 0;
+                    index = 1;
                 }
             } else {
                 index = 4;
             }
         }
+        
         // Check if player clicked on button 2
         else if (buttons[1].getText().equals("X")) {
             // If button 1 is not selected, make a move on button 1
@@ -248,6 +248,183 @@ public class NewTicTacToe implements ActionListener {
                 index = 0;
             }
         }
+        
+
+        // Check if player clicked on button 3
+        else if (buttons[2].getText().equals("X")) {
+            // If button 5 is not selected, make a move on button 5
+            if (!buttons[4].getText().equals("")) {
+                // If button 1 is not selected, make a move on button 1
+                if (!buttons[0].getText().equals("")) {
+                    // If button 3 is not selected, make a move on button 3
+                    if (!buttons[2].getText().equals("")) {
+                        // If none of the preferred moves are available, make a random move
+                        do {
+                            index = random.nextInt(9);
+                        } while (!buttons[index].getText().equals(""));
+                    } else {
+                        index = 2;
+                    }
+                } else {
+                    index = 0;
+                }
+            } else {
+                index = 4;
+            }
+        }
+        
+        
+        // Check if player clicked on button 4
+        else if (buttons[3].getText().equals("X")) {
+            // If button 5 is not selected, make a move on button 5
+            if (!buttons[4].getText().equals("")) {
+                // If button 1 is not selected, make a move on button 1
+                if (!buttons[0].getText().equals("")) {
+                    // If button 7 is not selected, make a move on button 7
+                    if (!buttons[6].getText().equals("")) {
+                        // If none of the preferred moves are available, make a random move
+                        do {
+                            index = random.nextInt(9);
+                        } while (!buttons[index].getText().equals(""));
+                    } else {
+                        index = 6;
+                    }
+                } else {
+                    index = 0;
+                }
+            } else {
+                index = 4;
+            }
+        }
+        
+        // Check if player clicked on button 5
+        else if (buttons[4].getText().equals("X")) {
+            // If button 1 is selected, make a move on button 9
+            if (buttons[0].getText().equals("X")) {
+                index = 8;
+            }
+            // If button 3 is selected, make a move on button 7
+            else if (buttons[2].getText().equals("X")) {
+                index = 6;
+            }
+            // If button 2 is selected, make a move on button 8
+            else if (buttons[1].getText().equals("X")) {
+                index = 7;
+            }
+            // If button 4 is selected, make a move on button 6
+            else if (buttons[3].getText().equals("X")) {
+                index = 5;
+            }
+            // If button 9 is selected, make a move on button 1
+            else if (buttons[8].getText().equals("X")) {
+                index = 0;
+            }
+            // If button 7 is selected, make a move on button 3
+            else if (buttons[6].getText().equals("X")) {
+                index = 2;
+            }
+            // If button 8 is selected, make a move on button 2
+            else if (buttons[7].getText().equals("X")) {
+                index = 1;
+            }
+            // If button 6 is selected, make a move on button 4
+            else if (buttons[5].getText().equals("X")) {
+                index = 3;
+            }
+        }
+        
+        // Check if player clicked on button 6
+        else if (buttons[5].getText().equals("X")) {
+            // If button 5 is not selected, make a move on button 5
+            if (!buttons[4].getText().equals("")) {
+                // If button 9 is not selected, make a move on button 9
+                if (!buttons[8].getText().equals("")) {
+                    // If button 3 is not selected, make a move on button 3
+                    if (!buttons[2].getText().equals("")) {
+                        // If none of the preferred moves are available, make a random move
+                        do {
+                            index = random.nextInt(9);
+                        } while (!buttons[index].getText().equals(""));
+                    } else {
+                        index = 2;
+                    }
+                } else {
+                    index = 8;
+                }
+            } else {
+                index = 4;
+            }
+        }
+        
+        // Check if player clicked on button 7
+        else if (buttons[6].getText().equals("X")) {
+            // If button 5 is not selected, make a move on button 5
+            if (!buttons[4].getText().equals("")) {
+                // If button 4 is not selected, make a move on button 4
+                if (!buttons[3].getText().equals("")) {
+                    // If button 8 is not selected, make a move on button 8
+                    if (!buttons[7].getText().equals("")) {
+                        // If none of the preferred moves are available, make a random move
+                        do {
+                            index = random.nextInt(9);
+                        } while (!buttons[index].getText().equals(""));
+                    } else {
+                        index = 7;
+                    }
+                } else {
+                    index = 3;
+                }
+            } else {
+                index = 4;
+            }
+        }
+        
+        // Check if player clicked on button 8
+        else if (buttons[7].getText().equals("X")) {
+            // If button 5 is not selected, make a move on button 5
+            if (!buttons[4].getText().equals("")) {
+                // If button 7 is not selected, make a move on button 7
+                if (!buttons[6].getText().equals("")) {
+                    // If button 9 is not selected, make a move on button 9
+                    if (!buttons[8].getText().equals("")) {
+                        // If none of the preferred moves are available, make a random move
+                        do {
+                            index = random.nextInt(9);
+                        } while (!buttons[index].getText().equals(""));
+                    } else {
+                        index = 8;
+                    }
+                } else {
+                    index = 6;
+                }
+            } else {
+                index = 4;
+            }
+        }
+        
+        // Check if player clicked on button 9
+        else if (buttons[8].getText().equals("X")) {
+            // If button 5 is not selected, make a move on button 5
+            if (!buttons[4].getText().equals("")) {
+                // If button 8 is not selected, make a move on button 8
+                if (!buttons[7].getText().equals("")) {
+                    // If button 6 is not selected, make a move on button 6
+                    if (!buttons[5].getText().equals("")) {
+                        // If none of the preferred moves are available, make a random move
+                        do {
+                            index = random.nextInt(9);
+                        } while (!buttons[index].getText().equals(""));
+                    } else {
+                        index = 5;
+                    }
+                } else {
+                    index = 7;
+                }
+            } else {
+                index = 4;
+            }
+        }
+
 
         // Default random move if not in the specified conditions
         if (index == -1) {
@@ -297,40 +474,47 @@ public class NewTicTacToe implements ActionListener {
             textfield.setText("Draw");
             disableButtons();
         } else {
-            if (vsComputerMode) {
-                checkForWin("O");
+        	if (vsComputerMode) {
+                checkForWin("O", "Computer");
             } else {
-                checkForWin("X");
-                checkForWin("O");
+                checkForWin("X", "Player 1");
+                checkForWin("O", "Player 2");
             }
         }
     }
 
-    private void checkForWin(String symbol) {
+    private void checkForWin(String symbol, String playerName) {
         for (int[] winCondition : WIN_CONDITIONS) {
             if (buttons[winCondition[0]].getText().equals(symbol) &&
                     buttons[winCondition[1]].getText().equals(symbol) &&
                     buttons[winCondition[2]].getText().equals(symbol)) {
                 highlightWinningCells(winCondition);
-                updateScore(symbol);
+                updateScore(playerName);
                 return;
             }
         }
     }
 
     private void highlightWinningCells(int[] winCondition) {
+        String winnerSymbol = buttons[winCondition[0]].getText();
         for (int index : winCondition) {
             buttons[index].setBackground(Color.YELLOW);
         }
         disableButtons();
-        textfield.setText(symbolToPlayer.get(buttons[winCondition[0]].getText()) + " wins");
+        String winnerName = symbolToPlayer.get(winnerSymbol);
+        textfield.setText(winnerName + " wins");
+        updateScore(winnerName);
     }
 
+    
     private void disableButtons() {
         for (int i = 0; i < 9; i++) {
             buttons[i].setEnabled(false);
         }
     }
+    
+    
+    
 
     public void resetGame() {
         for (int i = 0; i < 9; i++) {
@@ -340,25 +524,96 @@ public class NewTicTacToe implements ActionListener {
         }
         firstTurn();
     }
+    
+    private void resetScoreboard() {
+        if (vsComputerMode) {
+            scoreboard.put("Player", 0);
+            scoreboard.put("Computer", 0);
+            player1ScoreLabel.setText("Player (X): 0");
+            player2ScoreLabel.setText("Computer (O): 0");
+        } else {
+            scoreboard.put("Player 1", 0);
+            scoreboard.put("Player 2", 0);
+            player1ScoreLabel.setText("Player 1 (X): 0");
+            player2ScoreLabel.setText("Player 2 (O): 0");
+        }
+        updateScoreLabels();
+        resetCommon();
+    }
+    
+    
+    private void resetCommon() {
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setText("");
+            buttons[i].setEnabled(true);
+            buttons[i].setBackground(null);
+        }
+        firstTurn();
+    }
+    
+    
+    
 
-    public void updateScore(String winner) {
-        scoreboard.put(winner, scoreboard.getOrDefault(winner, 0) + 1);
+    private void updateScore(String winner) {
+        if (vsComputerMode) {
+            if (winner.equals("Computer")) {
+                scoreboard.put("Computer", scoreboard.getOrDefault("Computer", 0) + 1);
+            } else {
+                scoreboard.put("Player", scoreboard.getOrDefault("Player", 0) + 1);
+            }
+        } else {
+            if (winner.equals("Player 1")) {
+                scoreboard.put("Player 1", scoreboard.getOrDefault("Player 1", 0) + 1);
+            } else {
+                scoreboard.put("Player 2", scoreboard.getOrDefault("Player 2", 0) + 1);
+            }
+        }
         updateScoreLabels();
     }
 
     public void updateScoreLabels() {
-        player1ScoreLabel.setText("Player 1 (X): " + scoreboard.getOrDefault("X", 0));
-        player2ScoreLabel.setText("Player 2 (O): " + scoreboard.getOrDefault("O", 0));
+        if (vsComputerMode) {
+            player1ScoreLabel.setText("Player (X): " + scoreboard.getOrDefault("Player", 0));
+            player2ScoreLabel.setText("Computer (O): " + scoreboard.getOrDefault("Computer", 0));
+        } else {
+            player1ScoreLabel.setText("Player 1 (X): " + scoreboard.getOrDefault("Player 1", 0));
+            player2ScoreLabel.setText("Player 2 (O): " + scoreboard.getOrDefault("Player 2", 0));
+        }
         player1ScoreLabel.setForeground(Color.WHITE);
         player2ScoreLabel.setForeground(Color.WHITE);
     }
+    
+    private void updateScoreLabelsForComputerMode() {
+        player1ScoreLabel.setText("Player (X): 0");
+        player2ScoreLabel.setText("Computer (O): 0");
+    }
 
+    private void updateScoreLabelsForPlayerMode() {
+        player1ScoreLabel.setText("Player 1 (X): 0");
+        player2ScoreLabel.setText("Player 2 (O): 0");
+    }
+   
+    
+ 
+
+
+        
     private static final int[][] WIN_CONDITIONS = {
-            { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 }
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}
     };
 
-    private final Map<String, String> symbolToPlayer = Map.of("X", "Player 1", "O", "Player 2");
+    
 
+    private final Map<String, String> symbolToPlayer = createSymbolToPlayerMap();
+
+    private Map<String, String> createSymbolToPlayerMap() {
+        if (vsComputerMode) {
+            return Map.of("X", "Player", "O", "Computer");
+        } else {
+            return Map.of("X", "Player 1", "O", "Player 2");
+        }
+    }
+    
     public static void main(String[] args) {
         new NewTicTacToe();
     }
